5649b7e4615f7d144490119121fbbb9082b497d9
amr naser <amrnaser99>
2022-09-19 14:11:40 +0200

    add booking model

--- lib/services/repositories/repository.dart
+++ lib/services/repositories/repository.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:travinia/core/error/exceptions.dart';
+import 'package:travinia/models/booking_model.dart';
 import 'package:travinia/models/hotels_model.dart';
 import 'package:travinia/models/login_model.dart';
 import 'package:travinia/models/profile_model.dart';
@@ -24,6 +25,8 @@ abstract class Repository {
   });
 
   Future<Either<PrimaryServerException, FacilitiesModel>> getFacilities();
+
+  Future<Either<PrimaryServerException, BookingModel>> getBooking();
 }
 
 extension HandlingRequestResultFunction on Repository {
