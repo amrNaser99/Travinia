e12eb2d99170cc653ee47669b2ec023d2c051303
Mohamed Hesham <mohamedosky123@gmail.com>
2022-09-20 17:21:18 +0200

    fixing errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,39 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:travinia/core/app/bloc/app_cubit.dart';
 import 'package:travinia/core/app/bloc/bloc_observer.dart';
 import 'package:travinia/core/app/travinia_app.dart';
 import 'package:travinia/core/di/injection.dart';
+import 'package:travinia/presentation/home/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   init();
 
-  runApp(const TraviniaApp());
+  runApp(const MyApp());
   Bloc.observer = MyBlocObserver();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AppCubit>(
+          create: (_) => sl<AppCubit>()..getHotels(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(),
+        home: HomeScreen(),
+      ),
+    );
+  }
 }
