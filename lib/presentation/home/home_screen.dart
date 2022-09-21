dd0d3ecc4056c379ee7f6ef697f9f6ab5afe30de
Mohamed Hesham <mohamedosky123@gmail.com>
2022-09-21 02:04:39 +0200

    updating hotel_data_widget

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:travinia/core/app/bloc/app_cubit.dart';
 import 'package:travinia/core/app/bloc/app_state.dart';
-import 'package:travinia/presentation/home/widegts/hotels_list_widgets.dart';
+import 'package:travinia/presentation/home/widegts/hotel_data_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,32 +13,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // @override
-  // void initState() {
-  //   setState(() {});
-  //   super.initState();
-
-  //   AppCubit.get(context).getHotels();
-  // }
-
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AppCubit, AppStates>(
       listener: (context, state) {},
       builder: (context, state) {
+        List hotelData = BlocProvider.of<AppCubit>(context).hotels;
         if (state is HotelsLoadingState) {
           return Scaffold(
               backgroundColor: Colors.lightBlueAccent,
               body: Center(child: CircularProgressIndicator()));
         } else {}
         return Scaffold(
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              children: [
-                HotelslistWidegt(),
-              ],
-            ),
+          body: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    return HotelDataWidget(
+                      hotelName: hotelData[index].name,
+                      hotelAdress: hotelData[index].address,
+                      distance: 2.0, //ToDo: calculate HOTEL far distance
+                      hotelPrice: hotelData[index].price,
+                      hotelRating: hotelData[index].rate,
+                    );
+                  },
+                  itemCount: hotelData.length,
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      height: 20,
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         );
       },
